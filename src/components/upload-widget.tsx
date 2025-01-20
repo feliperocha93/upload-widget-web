97bffeda813ec685f6f3c40be435e29b61cfb7fa
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2025-01-20 18:38:19 -0300

    create button to toggle widget box

--- src/components/upload-widget.tsx
+++ src/components/upload-widget.tsx
@@ -1,18 +1,29 @@
+import * as Collapsible from '@radix-ui/react-collapsible';
 import { UploadWidgetDropzone } from './upload-widget-dropzone';
 import { UploadWidgetHeader } from './upload-widget-header';
 import { UploadWidgetUploadList } from './upload-widget-upload-list';
+import { UploadWidgetMinimizedButton } from './upload-widget-minimized-button';
+import { useState } from 'react';
 
 export function UploadWidget() {
+  const [isWidgetOpen, setIsWidgetOpen] = useState(false);
+
   return (
-    <div className='bg-zinc-900 w-full overflow-hidden max-w-[360px] rounded-xl shadow-shape'>
-      <UploadWidgetHeader />
-      <div className='flex flex-col gap-4 py-3'>
-        <UploadWidgetDropzone />
+    <Collapsible.Root onOpenChange={setIsWidgetOpen}>
+      <div className='bg-zinc-900 overflow-hidden w-[360px] rounded-xl shadow-shape'>
+        {!isWidgetOpen && <UploadWidgetMinimizedButton />}
 
-        <div className='h-px bg-zinc-800 border-t border-black/50 box-content' />
+        <Collapsible.Content>
+          <UploadWidgetHeader />
+          <div className='flex flex-col gap-4 py-3'>
+            <UploadWidgetDropzone />
 
-        <UploadWidgetUploadList />
+            <div className='h-px bg-zinc-800 border-t border-black/50 box-content' />
+
+            <UploadWidgetUploadList />
+          </div>
+        </Collapsible.Content>
       </div>
-    </div>
+    </Collapsible.Root>
   );
 }
