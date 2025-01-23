1d2e99e2753070daab22c18be47cf44566fc1cab
Felipe Rocha de Lucena <fee.rocha@hotmail.com>
2025-01-23 17:48:49 -0300

    compress image

--- src/store/uploads.ts
+++ src/store/uploads.ts
@@ -4,6 +4,7 @@ import { useShallow } from 'zustand/shallow';
 import { immer } from 'zustand/middleware/immer';
 import { uploadFileToStorage } from '../http/upload-file-to-storage';
 import { CanceledError } from 'axios';
+import { compressImage } from '../utils/compress-image';
 
 export type Upload = {
   name: string;
@@ -44,9 +45,16 @@ export const useUploads = create<UploadState, [['zustand/immer', never]]>(
       }
 
       try {
+        const compressedFile = await compressImage({
+          file: upload.file,
+          quality: 0.5,
+          maxHeight: 200,
+          maxWidth: 200,
+        });
+
         await uploadFileToStorage(
           {
-            file: upload.file,
+            file: compressedFile,
             onProgress: (sizeInBytes) => {
               updateUpload(uploadId, { uploadSizeInBytes: sizeInBytes });
             },
