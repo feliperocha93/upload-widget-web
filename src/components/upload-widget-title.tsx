f2034cccec0b0ed417311696cea6a20aa8704a99
Felipe Rocha De Lucena <felipe.lucena@quintoandar.com.br>
2025-02-26 21:50:23 -0300

    feat: configure vercel

--- src/components/upload-widget-title.tsx
+++ src/components/upload-widget-title.tsx
@@ -16,7 +16,7 @@ export function UploadWidgetTitle() {
           </span>
         </span>
       ) : (
-        <span>Upload files 🚀</span>
+        <span>Upload files Vercel</span>
       )}
     </div>
   );
